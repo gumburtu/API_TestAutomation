fdad3eb382ee93a8c2c00317e7eb0416d28d33b4
gumburtu <aslanseval1986@gmail.com>
2025-03-13 10:54:52 +0000

    Base-Write

---
+++ src/test/java/C1_GetRequest_ResponseBody_Write.java
@@ -0,0 +1,26 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C1_GetRequest_ResponseBody {
+    @Test
+    public void get01() {
+        // https://restful-booker.herokuapp.com/booking/10 url'sine
+
+        //bir get request gonderdigimizde donen responsu'i yazdiriniz.
+
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+        // 1- Request body ve end -point hazirlama
+
+        // 2- Expected data hazirlama
+
+        // 3- Request gonderip, donen response'i kaydetme
+
+        Response response = given().when().get(url);
+        response.prettyPrint();
+
+        // 4- Assertion
+    }
+}
